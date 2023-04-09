5635581f1b6c777a2dd500719321c3fdae770763
Laurie Fish <iamlauriefish@gmail.com>
2023-04-08 22:07:37 -0700

    added getCurrentWeather function and event listener

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 var APIkey = "19a8ea7d231d294d32f52b217ae8e55c";
-
-
+var clear = document.getElementById("clear");
+var inputButton = document.getElementById('input-button');
 var currentTempEl = document.getElementById('temp');
 var currentWindEl = document.getElementById('wind');
 var currentHumidity = document.getElementById('humidity');
-var city = "Portland";
+var city = "";
 var date = dayjs().format('dddd MMMM D YYYY   h:mm: a');
+
+var showOldEntries = document.getElementById('old-inputs');
+var historyEl = document.getElementById('history');
 
 
 
@@ -22,13 +25,16 @@ function getCurrentWeather(event) {
         console.log(queryURL);
         console.log(response);
 
-        $(".city-name").html("<h2>Currently in " + response.name + ":" + " " + date + "</h2>");
+        $("#city-name").html("<h2>Currently in " + response.name + ":" + " " + date + "</h2>");
         $("#wind").html("<p>Wind Speed: " + response.wind.speed + "</p>");
         $("#humidity").text("Humidity: " + response.main.humidity);
         $("#temp").text("Temperature (F) " + response.main.temp);
 
     });
+    $("#input-button").on("click", (event) => {
+        city = $("#search-city").val();
 
+    });
 
 
 
