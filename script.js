f48c6ea7159379a0f615ac59557b9d2b94a19b0b
Laurie Fish <iamlauriefish@gmail.com>
2023-04-09 18:39:43 -0700

    added for loop for five-day and local storage

--- script.js
+++ script.js
@@ -5,47 +5,76 @@ var currentTempEl = document.getElementById('temp');
 var currentWindEl = document.getElementById('wind');
 var currentHumidity = document.getElementById('humidity');
 var city = document.getElementById("search-city");
-
+//var city = "portland";
 var date = dayjs().format('dddd MMMM D YYYY   h:mm: a');
 
 var showOldEntries = document.getElementById('old-inputs');
 var historyEl = document.getElementById('history');
 
+var handleErrors = (response) => {
+    if (!response.ok) {
+        throw Error(response.statusText);
+    }
+    return response;
+}
 
 
 
 
+function getCurrentWeather() {
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city.value + "&units=imperial&appid=" + APIkey;
 
-function getCurrentWeather(city) {
-    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIkey;
+    fetch(queryURL)
+        .then(handleErrors)
+        .then((response) => {
+            return response.json();
+        })
+        .then((response) => {
+            console.log(queryURL);
+            console.log(response);
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(queryURL);
-        console.log(response);
+            $("#city-name").html("<h2>Currently in " + response.name + ":" + " " + date + "</h2>");
+            $("#wind").html("<p>Wind Speed: " + response.wind.speed + "</p>");
+            $("#humidity").text("Humidity: " + response.main.humidity);
+            $("#temp").text("Temperature (F) " + response.main.temp);
 
-        $("#city-name").html("<h2>Currently in " + response.name + ":" + " " + date + "</h2>");
-        $("#wind").html("<p>Wind Speed: " + response.wind.speed + "</p>");
-        $("#humidity").text("Humidity: " + response.main.humidity);
-        $("#temp").text("Temperature (F) " + response.main.temp);
-
-    });
+        });
 }
 function getFiveDayForecast(event) {
     let newCity = city.value;
-    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIkey;
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        let forecastBox = document.querySelectorAll(".five-day-box");
-        for (i = 0; i < forecastBox.length; i++) {
-            forecastBox[i].innerHTML = "";
+    let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city.value + "&units=imperial&appid=" + APIkey;
+    fetch(queryURL)
+        .then(handleErrors)
+        .then((response) => {
+            return response.json();
+        })
+        .then((response) => {
+            console.log(queryURL);
+            console.log(response.list);
 
-        }
-    })
+            let forecastBox = document.querySelectorAll(".forecast");
+            let dayNumber = 0;
+            for (i = 0; i < response.list.length; i++) {
+                let dayData = response.list[i];
+                let dayTimeUTC = dayData.dt;
+                let timeZoneOffset = response.city.timezone;
+                let timeZoneOffsetHours = timeZoneOffset / 60 / 60;
+                let thisMoment = moment.unix(dayTimeUTC).utc().utcOffset(timeZoneOffsetHours);
+                //console.log(thisMoment);
+                if (thisMoment.format("HH:mm:ss") === "11:00:00" || thisMoment.format("HH:mm:ss") === "12:00:00" || thisMoment.format("HH:mm:ss") === "13:00:00") {
+                    switch (dayNumber) {
+                        case 0:
+                            let htmlString = 'Temperature' + response.list[i].main.temp;
+                            htmlString += 'Wind Speed' + response.list[i].wind.speed;
+                            htmlString += 'Humidity' + response.list[i].main.humidity;
+
+                            document.getElementById("five-day-one").innerHTML = htmlString;
+
+                    }
+                }
+
+            }
+        })
 }
 
 
@@ -54,13 +83,16 @@ function getFiveDayForecast(event) {
 $("#input-button").on("click", (event) => {
     let newCity = city.value;
     console.log(newCity);
-    getCurrentWeather(newCity);
+    getCurrentWeather();
     var searchCity = $("#old-inputs")
     searchCity.attr('id', newCity)
     searchCity.text(newCity)
     searchCity.addClass("h4")
+
+    let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city.value + "&units=imperial&appid=" + APIkey;
+    localStorage.setItem(newCity, queryURL);
+    getFiveDayForecast();
 });
-//getCurrentWeather();
 
 
 
