9704520d5b2aef827f8d2381a8e26e8426838057
Laurie Fish <iamlauriefish@gmail.com>
2023-04-10 07:06:36 -0700

    added render function

--- script.js
+++ script.js
@@ -7,9 +7,10 @@ var currentHumidity = document.getElementById('humidity');
 var city = document.getElementById("search-city");
 //var city = "portland";
 var date = dayjs().format('dddd MMMM D YYYY   h:mm: a');
-
+let stringList = JSON.parse(localStorage.getItem("stringList")) || [];
 var showOldEntries = document.getElementById('old-inputs');
 var historyEl = document.getElementById('history');
+
 
 var handleErrors = (response) => {
     if (!response.ok) {
@@ -40,6 +41,15 @@ function getCurrentWeather() {
 
         });
 }
+function addCity() {
+    let newCity = city.value;
+    var searchCity = $("<li></li>");
+    searchCity.attr('id', newCity)
+    searchCity.text(newCity)
+    searchCity.addClass("h4")
+    $("#previous-cities").append(searchCity);
+
+}
 function getFiveDayForecast(event) {
     let newCity = city.value;
     let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city.value + "&units=imperial&appid=" + APIkey;
@@ -60,38 +70,50 @@ function getFiveDayForecast(event) {
                 let timeZoneOffset = response.city.timezone;
                 let timeZoneOffsetHours = timeZoneOffset / 60 / 60;
                 let thisMoment = moment.unix(dayTimeUTC).utc().utcOffset(timeZoneOffsetHours);
+
                 //console.log(thisMoment);
                 if (thisMoment.format("HH:mm:ss") === "11:00:00" || thisMoment.format("HH:mm:ss") === "12:00:00" || thisMoment.format("HH:mm:ss") === "13:00:00") {
                     switch (dayNumber) {
                         case 0:
-                            let htmlString = 'Temperature' + response.list[i].main.temp;
-                            htmlString += 'Wind Speed' + response.list[i].wind.speed;
-                            htmlString += 'Humidity' + response.list[i].main.humidity;
+
+                            let htmlString = 'Temperature ' + response.list[i].main.temp;
+
+                            htmlString += '      Wind Speed ' + response.list[i].wind.speed;
+                            htmlString += 'Humidity ' + response.list[i].main.humidity;
+                            htmlString += response.list[i].weatherIcon;
+
+
+
+                            // htmlString += 'Wind Speed' + response.list[i].wind.speed;
+                            // htmlString += 'Humidity' + response.list[i].main.humidity;
+                            // htmlString += response.list[i].weather.icon;
 
                             document.getElementById("five-day-one").innerHTML = htmlString;
-
                     }
                 }
-
             }
         })
 }
 
 
-
-
 $("#input-button").on("click", (event) => {
     let newCity = city.value;
     console.log(newCity);
+
     getCurrentWeather();
-    var searchCity = $("#old-inputs")
-    searchCity.attr('id', newCity)
-    searchCity.text(newCity)
-    searchCity.addClass("h4")
 
     let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city.value + "&units=imperial&appid=" + APIkey;
+
+    // let searchCity = $("#old-inputs")
+    // searchCity.attr('id', newCity)
+    // searchCity.text(newCity)
+    // searchCity.addClass("h4")
+    // $("#list-group").append(searchCity);
     localStorage.setItem(newCity, queryURL);
+
+
     getFiveDayForecast();
+    addCity();
 });
 
 
